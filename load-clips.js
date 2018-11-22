ec9b8702b10a7b37e18ab11fb0d7c6b7445354ae
latiertiw <latierti@yandex.ru>
2018-11-23 00:37:50 +0300

    *

--- load-clips.js
+++ load-clips.js
@@ -2,26 +2,35 @@ block=document.querySelector('.search-result-block .searched-videos');
 button=document.querySelector('.search-block .search-button');
 outBlock=document.querySelector('.video-block .output-video-block');
 searchInput=document.querySelector('.search-block .search-input');
+pageRight=document.querySelector('.page-switcher .page-right');
+pageLeft=document.querySelector('.page-switcher .page-left');
+numberOfPages=document.querySelector('.page-switcher .current-page');
 
-let baseUrl ="https://www.googleapis.com/youtube/v3/search?key=AIzaSyC0vey-MWqac8d52xu5VWF1r6q3e59xI0Q&type=video&part=snippet&maxResults=3";
+let baseUrl ="https://www.googleapis.com/youtube/v3/search?key=AIzaSyC0vey-MWqac8d52xu5VWF1r6q3e59xI0Q&type=video&part=snippet&maxResults=8";
 let baseSUrl='https://www.googleapis.com/youtube/v3/videos?key=AIzaSyC0vey-MWqac8d52xu5VWF1r6q3e59xI0Q&id=';
 let Url;
 //(document.body.clientWidth);
 
 let bufferedVideos=[];
+let pages=[];
 let loadedCount=0;
 let nextPageToken='';
+let lastSearch;
+let currentPage=0;
+
 
 
 button.addEventListener("click", search);
-function search(){
-    Url=baseUrl+'&pageToken='+nextPageToken+'&q='+searchInput.value;
+window.addEventListener('resize', resize);
+
+function load(){
+    Url=baseUrl+'&pageToken='+nextPageToken+'&q='+lastSearch;
     fetch(Url)
     .then(res => res.text())
     .then(res=>{    
        let answer=JSON.parse(res);
        nextPageToken=answer.nextPageToken;
-       for(let loadQuerry=0;loadQuerry<3;loadQuerry++){
+       for(let loadQuerry=0;loadQuerry<8;loadQuerry++){
 
         let card=new Object();
 
@@ -49,48 +58,118 @@ function search(){
        }
 });
 }
+
+function search(){
+    pageRight.addEventListener("click", nextPage);
+    pageLeft.addEventListener("click", prevPage);
+    bufferedVideos=[];
+    nextPageToken='';
+    lastSearch=searchInput.value;
+    Url=baseUrl+'&pageToken='+nextPageToken+'&q='+searchInput.value;
+    fetch(Url)
+    .then(res => res.text())
+    .then(res=>{    
+       let answer=JSON.parse(res);
+       nextPageToken=answer.nextPageToken;
+       for(let loadQuerry=0;loadQuerry<8;loadQuerry++){
+
+        let card=new Object();
+
+        card.imageSource=answer.items[loadQuerry].snippet.thumbnails.medium.url
+        card.channelTitle=answer.items[loadQuerry].snippet.channelTitle;
+        card.videoDescription=answer.items[loadQuerry].snippet.description;
+        card.videoTitle=answer.items[loadQuerry].snippet.title;
+        card.publishedTime=answer.items[loadQuerry].snippet.publishedAt;
+        card.videoId=answer.items[loadQuerry].id.videoId;
+        
+            Url=baseSUrl+card.videoId+'&part=snippet,statistics';
+            fetch(Url)
+            .then(res => res.text())
+            .then(res => {
+                let answer=JSON.parse(res);
+            
+                card.viewCount=answer.items[0].statistics.viewCount;
+                card.likeCount=answer.items[0].statistics.likeCount;
+                card.dislikeCount=answer.items[0].statistics.dislikeCount;
+
+                bufferedVideos.push(card);
+        })
+        
+        loadedCount++;
+       }
+})
+}
+
 function cleanSearchBlock(){
     while(block.firstChild){
         block.removeChild(block.firstChild);
     }
 }
 
-function fill(){
-    cleanSearchBlock();
-    if(document.body.clientWidth>1000){
-        add(8)
+function createPages(size){
+    let pagesCount=Math.ceil(bufferedVideos.length/size);
+    pages=[];
+    
+
+    let placed=0;
+    for(let i=0;i<pagesCount;i++){
+        pages[i]=[];
+        for(let j=0; j<size;j++){
+            if(placed<bufferedVideos.length){
+                pages[i].push(bufferedVideos[placed]);
+                placed++;
+            }
+        }
     }
-    else if(document.body.clientWidth>800){add(6)}
-    else if(document.body.clientWidth>730){add(4)}
-    else if(document.body.clientWidth>550){add(3)}
-    else{add(2)}
+   // console.log(pagesCount)
+}
+
+function renderPage(number){
+    for(let i=0;i<pages[number].length;i++){
+        item=document.createElement('div');
+        item.className='searched-video';
+        block.appendChild(item);
+    }
+}
+
+function nextPage(){
+    currentPage += 1;
+    if(currentPage>pages.length){
+        load();
+    }
+    resize();
+}
+
+function prevPage(){
+    if(currentPage !==0 ){
+        currentPage -= 1;
+    }
+    resize();
 }
 
 
-function add(count){
-for(let i=0;i<count;i++){
-    item=document.createElement('div');
-    item.className='searched-video';
-    block.appendChild(item);
-}
-}
-
-
-
-
-
-
-
-window.addEventListener('resize', resize);
 
   function resize(){
-     // console.log(document.body.clientWidth)
      cleanSearchBlock();
+     numberOfPages.textContent=currentPage+1;
      if(document.body.clientWidth>1000){
-         add(8)
+         createPages(8);
+         renderPage(currentPage);
      }
-     else if(document.body.clientWidth>800){add(6)}
-     else if(document.body.clientWidth>730){add(4)}
-     else if(document.body.clientWidth>550){add(3)}
-     else{add(2)}
+     else if(document.body.clientWidth>800){
+         createPages(6);
+         renderPage(currentPage);
+    }
+     else if(document.body.clientWidth>730){
+         createPages(4);
+         renderPage(currentPage);
+    }
+     else if(document.body.clientWidth>550){
+         createPages(3)
+         renderPage(currentPage);
+        }
+     else{
+         createPages(2);
+         renderPage(currentPage);
+        }
   }
